ee7dfb1f5a8790a873122e9b6ec419e7bfdab21f
gabriel.castillo86@gmail.com <gabriel.castillo86@gmail.com@62347f08-01f6-2b61-4396-82cf66d872eb>
2014-08-25 23:01:26 +0000

    Reorganizar scripts

--- Codigo/LoginCol.Huellitas/LoginCol.Huellitas.Web/Controllers/AnimalController.cs
+++ Codigo/LoginCol.Huellitas/LoginCol.Huellitas.Web/Controllers/AnimalController.cs
@@ -1,41 +1,16 @@
-﻿using LoginCol.Huellitas.Web.Infraestructure;
-using LoginCol.Huellitas.Web.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Net.Http;
+﻿using System.Net.Http;
 using System.Web.Http;
-using System.Web.Mvc;
 
 namespace LoginCol.Huellitas.Web.Controllers
 {
-    public class AnimalController : Controller, ICrudAdministracion<AnimalModel>
+    public class AnimalController : ApiController
     {
-
-        public List<AnimalModel> Listar()
+        [HttpGet]
+        public HttpResponseMessage Get()
         {
-            throw new NotImplementedException();
-        }
-
-        public AnimalModel Obtener(int id)
-        {
-            throw new NotImplementedException();
-        }
-
-        public AnimalModel Crear(int id, AnimalModel modelo)
-        {
-            throw new NotImplementedException();
-        }
-
-        public AnimalModel Editar(int id, AnimalModel modelo)
-        {
-            throw new NotImplementedException();
-        }
-
-        public AnimalModel Eliminar(int id)
-        {
-            throw new NotImplementedException();
+            
+            
+            //return Request.CreateResponse(HttpStatusCode.OK, );
         }
     }
 }
