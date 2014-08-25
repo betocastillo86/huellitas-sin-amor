cb2f10579186807af84d837456ed4098aa60ebdb
gabriel.castillo86@gmail.com <gabriel.castillo86@gmail.com@62347f08-01f6-2b61-4396-82cf66d872eb>
2014-08-25 23:01:26 +0000

    Reorganizar scripts

--- trunk/Codigo/LoginCol.Huellitas/LoginCol.Huellitas.Web/App_Start/BundleConfig.cs
+++ trunk/Codigo/LoginCol.Huellitas/LoginCol.Huellitas.Web/App_Start/BundleConfig.cs
@@ -10,28 +10,28 @@ namespace LoginCol.Huellitas.Web
         {
            
             bundles.Add(new ScriptBundle("~/bundles/backbone").Include(
-                "~/Scripts/json2.js",
-                "~/Scripts/underscore.js" ,
-                "~/Scripts/backbone.js"
+                "~/Scripts/libs/json2.js",
+                "~/Scripts/libs/underscore.js",
+                "~/Scripts/libs/backbone.js"
                         
                 ));
             
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                        "~/Scripts/libs/jquery-{version}.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryui").Include(
-                        "~/Scripts/jquery-ui-{version}.js"));
+                        "~/Scripts/libs/jquery-ui-{version}.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.unobtrusive*",
-                        "~/Scripts/jquery.validate*"));
+                        "~/Scripts/libs/jquery.unobtrusive*",
+                        "~/Scripts/libs/jquery.validate*"));
 
             
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
+                        "~/Scripts/libs/modernizr-*"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/site.css"));
 
