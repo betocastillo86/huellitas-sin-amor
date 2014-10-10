2cf08c1b95500d01718ed7756b7b55a538feb977
gabriel.castillo86@gmail.com <gabriel.castillo86@gmail.com@62347f08-01f6-2b61-4396-82cf66d872eb>
2014-10-10 04:06:46 +0000

    Faltantes archivos version anterior

---
+++ trunk/Codigo/LoginCol.Huellitas/LoginCol.Huellitas.Utilidades/Seguridad.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoginCol.Huellitas.Utilidades
+{
+    public class Seguridad
+    {
+        /// <summary>
+        /// Genera una cadena encriptada en MD5
+        /// </summary>
+        /// <param name="input">valor que se desea encriptar</param>
+        /// <returns>cadena encriptada en md5</returns>
+        public static string MD5(string input)
+        {
+            MD5CryptoServiceProvider x = new System.Security.Cryptography.MD5CryptoServiceProvider();
+            byte[] bs = System.Text.Encoding.UTF8.GetBytes(input);
+            bs = x.ComputeHash(bs);
+            System.Text.StringBuilder s = new System.Text.StringBuilder();
+            foreach (byte b in bs)
+            {
+                s.Append(b.ToString("x2").ToLower());
+            }
+            string password = s.ToString();
+            return password;
+        }
+    }
+}
---
+++ trunk/Codigo/LoginCol.Huellitas/LoginCol.Huellitas.Web/Infraestructure/Autenticacion.cs
@@ -0,0 +1,66 @@
+﻿using LoginCol.Huellitas.Entidades;
+using LoginCol.Huellitas.Negocio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+
+namespace LoginCol.Huellitas.Web.Infraestructure
+{
+    public class Autenticacion
+    {
+        /// <summary>
+        /// Valida las credenciales de un usuario y crea la sesion
+        /// </summary>
+        /// <param name="correo"></param>
+        /// <param name="clave"></param>
+        /// <param name="validarAdmin">true: Solo permite autenticar administradores false: permite autenticar a todos los usuarios</param>
+        /// <returns></returns>
+        public bool AutenticarUsuario(string correo, string clave, bool validarAdmin)
+        {
+            UsuarioNegocio usuariosNegocio = new UsuarioNegocio();
+            Usuario usuarioAutenticado = usuariosNegocio.AutenticarUsuario(correo, clave);
+
+            if (usuarioAutenticado.UsuarioId > 0)
+            {
+                if (!validarAdmin || (validarAdmin && usuarioAutenticado.EsAdministrador))
+                {
+                    Autenticacion.CrearSesion(usuarioAutenticado);
+                    return true;
+                }
+                else
+                    return false;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+
+
+        public static bool EstaAutenticado()
+        {
+            return HttpContext.Current.User.Identity.IsAuthenticated;
+        }
+
+        /// <summary>
+        /// Cierra la sesión activa
+        /// </summary>
+        public static void CerrarSesion()
+        {
+            FormsAuthentication.SignOut();
+        }
+
+        /// <summary>
+        /// Crea la cookie de autenticación para el usuario recibido
+        /// </summary>
+        /// <param name="usuarioAutenticado"></param>
+        public static void CrearSesion(Usuario usuarioAutenticado)
+        {
+            string cookie = string.Format("{0}|{1}|{2}|{3}|{4}|{5}", usuarioAutenticado.UsuarioId, usuarioAutenticado.Nombres, usuarioAutenticado.Apellidos, usuarioAutenticado.Correo, usuarioAutenticado.NumeroDocumento, usuarioAutenticado.EsAdministrador);
+            FormsAuthentication.SetAuthCookie(cookie, true);
+        }
+    }
+}
