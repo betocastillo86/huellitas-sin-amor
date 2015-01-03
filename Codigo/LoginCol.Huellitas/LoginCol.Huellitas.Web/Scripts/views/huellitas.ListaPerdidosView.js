fc2a92bffbec78a58d58ab5b20c470aa81bd7bce
gabriel.castillo86@gmail.com <gabriel.castillo86@gmail.com@62347f08-01f6-2b61-4396-82cf66d872eb>
2015-01-03 18:43:49 +0000

    perddo

---
+++ Codigo/LoginCol.Huellitas/LoginCol.Huellitas.Web/Scripts/views/huellitas.ListaPerdidosView.js
@@ -0,0 +1,36 @@
+﻿var ListaPerdidosView =  Backbone.View.extend({
+
+
+    template: _.template(this.$("#templateResultadosBusqueda").html()),
+
+    models: undefined,
+
+    opcionesFiltro: undefined,
+
+    initialize: function (args) {
+
+        if (args.opcionesFiltro)
+            this.opcionesFiltro = args.opcionesFiltro;
+
+        this.models = new ContenidoCollection();
+        this.models.on("sync", this.mostrarContenidos, this);
+    },
+
+    cargarContenidos: function (opcionesFiltro) {
+
+
+        if (opcionesFiltro)
+            this.opcionesFiltro = opcionesFiltro;
+
+        opcionesFiltro.tipoFiltroBase = Constantes.TipoAnimalesPerdidosPadre;
+
+        this.models.cargarHuellitasPorFiltro(opcionesFiltro);
+
+    },
+
+    mostrarContenidos: function (models) {
+        this.$("#divResultadosBusqueda").html(this.template(models.toJSON()));
+    }
+
+    
+});
