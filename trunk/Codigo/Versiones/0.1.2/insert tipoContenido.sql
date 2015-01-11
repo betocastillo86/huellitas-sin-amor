889cccf1ab727aa1351bcaea626322a6f94beab5
gabriel.castillo86@gmail.com <gabriel.castillo86@gmail.com@62347f08-01f6-2b61-4396-82cf66d872eb>
2015-01-11 17:16:06 +0000

    Se agrega descripcion a perdidos

---
+++ trunk/Codigo/Versiones/0.1.2/insert tipoContenido.sql
@@ -0,0 +1,17 @@
+----TIPO CONTENIDO
+8	Animales Perdidos	NULL
+9	Encontrados	8
+10	Perdidos	8
+
+--CAMPO
+21	Nombre Contacto	3	NULL	True
+22	Telefono Contacto	3	NULL	True
+23	Correo Contacto	3	NULL	True
+24	Tipo Perdido	1	NULL	True
+
+--PARAMETRIZACION
+CampoContactoNombreId	21
+CampoContactoCorreoId	23
+CampoContactoTelefonoId	22
+UsuarioPorDefecto	1
+CampoTipoPerdido	24
