5aaabcacfb8c452b285dfd23f017d565ce2f119a
gabriel.castillo86@gmail.com <gabriel.castillo86@gmail.com@62347f08-01f6-2b61-4396-82cf66d872eb>
2015-03-22 02:30:17 +0000

    faltaba archivo

---
+++ Codigo/LoginCol.Huellitas/LoginCol.Huellitas.Web/Controllers/AdminAdopcionesController.cs
@@ -0,0 +1,44 @@
+﻿using LoginCol.Huellitas.Negocio;
+using LoginCol.Huellitas.Utilidades;
+using LoginCol.Huellitas.Web.Models;
+using LoginCol.Huellitas.Web.Models.Admin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace LoginCol.Huellitas.Web.Controllers
+{
+    
+    public class AdminAdopcionesController : ApiController
+    {
+
+        
+        /// <summary>
+        /// Actualiza los datos de un formulario de adopción. Inicialmente la funcionalidad solo aplica apra envio de correos
+        /// </summary>
+        /// <param name="modelo">Modelo de formulario de adopción con los datos a atualizar</param>
+        /// <param name="enviarCorreo">variable por URL que valida si se debe enviar el correo o no</param>
+        /// <returns>Resultado de operación con toda la información</returns>
+        [HttpPut]
+        public ResultadoOperacion Put(FormularioAdopcionModelBase modelo, [FromUri] bool enviarCorreo = false)
+        {
+            ResultadoOperacion respuesta = new ResultadoOperacion();
+            
+            var nAdopciones = new FormularioAdopcionNegocio();
+            
+            //Por ahora solo funciona para envío de correos y se hace la actualización interna
+            if(enviarCorreo)
+            {
+                respuesta = nAdopciones.EnviarRespuestaAdopcion(modelo.FormularioAdopcionId,
+                    modelo.Estado, 
+                    modelo.Observaciones, 
+                    modelo.InformacionAdicionalCorreo);
+            }
+
+            return respuesta;
+        }
+    }
+}
